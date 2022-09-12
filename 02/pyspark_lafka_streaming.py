d8d946f902ab15d11268fa9f3a56367e6aba68bd
rivindu2 <rivindu@incubatelabs.com>
2022-09-12 13:22:22 +0530

    Fixed Errors

--- 02/pyspark_lafka_streaming.py
+++ 02/pyspark_lafka_streaming.py
@@ -5,14 +5,14 @@ from pyspark.sql.types import *
 
 import time
 
-kafka_topic_name = "orderstopic"
+kafka_topic_name = "ordersTopic"
 KAFKA_BOOTSTRAP_SERVERS_CONS = 'localhost:9092'
 customers_data_file_path = "customers.csv"
 
 mysql_host_name = "localhost"
 mysql_port_no = "3306"
 mysql_database_name = "sales_db"
-mysql_driver_class = "com.mysql.jdbc.Driver"
+mysql_driver_class = "com.mysql.cj.jdbc.Driver"
 mysql_table_name = "total_sales_by_source_state"
 mysql_user_name = "root"
 mysql_password = "mynewpassword"
@@ -44,21 +44,22 @@ def save_to_mysql(current_df, epoc_id):
                       "driver": mysql_driver_class}
     print("Printing epoc_id: ")
     print(epoc_id)
-    print(current_df.printSchema())
+    current_df.show()
     processed_at = time.strftime("%Y-%m-%d %H:%M:%S")
 
     current_df_final = current_df \
         .withColumn("processed_at", lit(processed_at)) \
-        .withColumn("batch_it", lit(epoc_id))
+        .withColumn("batch_id", lit(epoc_id))
     print(current_df_final.printSchema())
     print("Printing before Msql table save: " + str(epoc_id))
     current_df_final.show()
     current_df_final \
-        .write() \
+        .write \
         .format("jdbc") \
         .option("url", "jdbc:mysql://localhost:3306/sales_db?characterEncoding=UTF-8") \
         .option("dbtable", "total_sales_by_source_state") \
         .option("user", "root") \
+        .option("driver", "com.mysql.cj.jdbc.Driver") \
         .option("password", "mynewpassword") \
         .mode("append") \
         .save()
@@ -80,6 +81,7 @@ if __name__ == "__main__":
                 "con/spark-cassandra-connector-2.4.0-s_2.11.jar,"
                 "jars/spark-streaming-kafka-0-10_2.12-3.2.1.jar,"
                 "jars/mysql-connector-java-8.0.29.jar,"
+                "jars/commons-pool2-2.11.1.jar,"
                 "jars/spark-token-provider-kafka-0-10_2.12-3.3.0.jar,"
                 "jars/spark-sql-kafka-0-10_2.12-3.2.1.jar,"
                 "jars/kafka-clients-3.2.1.jar") \
@@ -87,6 +89,7 @@ if __name__ == "__main__":
                 "con/jsr166e-1.0.0.jar,"
                 "con/spark-cassandra-connector-2.4.0-s_2.11.jar,"
                 "jars/spark-streaming-kafka-0-10_2.12-3.2.1.jar,"
+                "jars/commons-pool2-2.11.1.jar,"
                 "jars/spark-token-provider-kafka-0-10_2.12-3.3.0.jar,"
                 "jars/mysql-connector-java-8.0.29.jar,"
                 "jars/spark-sql-kafka-0-10_2.12-3.2.1.jar,"
@@ -95,6 +98,7 @@ if __name__ == "__main__":
                 "con/jsr166e-1.0.0.jar, "
                 "con/spark-cassandra-connector-2.4.0-s_2.11.jar,"
                 "jars/spark-streaming-kafka-0-10_2.12-3.2.1.jar,"
+                "jars/commons-pool2-2.11.1.jar,"
                 "jars/spark-token-provider-kafka-0-10_2.12-3.3.0.jar,"
                 "jars/spark-sql-kafka-0-10_2.12-3.2.1.jar,"
                 "jars/mysql-connector-java-8.0.29.jar,"
@@ -103,6 +107,7 @@ if __name__ == "__main__":
                 "con/jsr166e-1.0.0.jar, "
                 "con/spark-cassandra-connector-2.4.0-s_2.11.jar,"
                 "jars/spark-streaming-kafka-0-10_2.12-3.2.1.jar,"
+                "jars/commons-pool2-2.11.1.jar,"
                 "jars/spark-token-provider-kafka-0-10_2.12-3.3.0.jar,"
                 "jars/spark-sql-kafka-0-10_2.12-3.2.1.jar,"
                 "jars/mysql-connector-java-8.0.29.jar,"
@@ -122,6 +127,10 @@ if __name__ == "__main__":
         .option("startingOffsets", "latest") \
         .load()
 
+    print(("orders_df1 = orders_df"))
+    print((type(orders_df)))
+    print(orders_df.__dict__)
+
     print("Printing Schema of transaction_detail_df: ")
     orders_df.printSchema()
 
@@ -139,19 +148,21 @@ if __name__ == "__main__":
         .add("total", StringType()) \
         .add("customer_id", StringType())
 
+    # orders_df1.writeStream.format("console").outputMode("append").start().awaitTermination()
+
     orders_df2 = orders_df1 \
         .select(from_json(col("value"), orders_schema).alias("orders"), "timestamp")
     print("SSSSSSSSSSSSSSSSSSSSSSSSSs 1")
 
     orders_df3 = orders_df2.select("orders.*", "timestamp")
     print("SSSSSSSSSSSSSSSSSSSSSSSSSs 2")
-    orders_df3 \
-        .writeStream \
-        .trigger(processingTime='15 seconds') \
-        .outputMode("update") \
-        .foreachBatch(save_to_cassandra) \
-        .start()
-    print(orders_df3.printSchema(), " SSSSSSSSSSSSSSSSSSSSSSSSSs3")
+    # orders_df3 \
+    #     .writeStream \
+    #     .trigger(processingTime='15 seconds') \
+    #     .outputMode("update") \
+    #     .foreachBatch(save_to_cassandra) \
+    #     .start()
+    # print(orders_df3.printSchema(), " SSSSSSSSSSSSSSSSSSSSSSSSSs3")
     customers_df = spark.read.csv(customers_data_file_path, header=True, inferSchema=True)
     customers_df.printSchema()
     customers_df.show(5, False)
@@ -163,30 +174,32 @@ if __name__ == "__main__":
     orders_df4 = orders_df3.join(customers_df, orders_df3.customer_id == customers_df.customer_id, how='inner')
     print("Printing Schema of orders_df4 ")
     orders_df4.printSchema()
-
+    # orders_df4.writeStream.format("console").outputMode("append").start().awaitTermination()
+    # orders_df4.awaitTermination()
     orders_df5 = orders_df4.groupBy("source", "state") \
         .agg({'total': 'sum'}).select("source", "state", col("sum(total)").alias("total_sum_amount"))
 
     print("Printing Schema of orders_df5: ")
     orders_df5.printSchema()
+    # orders_df5.writeStream.format("console").outputMode("append").start().awaitTermination()
 
-    # Write final result into console for debugging purpose
+# Write final result into console for debugging purpose
 
-    trans_detail_write_stream = orders_df5 \
-        .writeStream \
-        .trigger(processingTime='15 seconds') \
-        .outputMode("update") \
-        .option("truncate", "false") \
-        .format("console") \
-        .start()
+trans_detail_write_stream = orders_df5 \
+    .writeStream \
+    .trigger(processingTime='15 seconds') \
+    .outputMode("update") \
+    .option("truncate", "false") \
+    .format("console") \
+    .start()
 
-    orders_df5 \
-        .writeStream \
-        .trigger(processingTime='15 seconds') \
-        .outputMode("update") \
-        .foreachBatch(save_to_mysql) \
-        .start()
+orders_df5 \
+    .writeStream \
+    .trigger(processingTime='15 seconds') \
+    .outputMode("update") \
+    .foreachBatch(save_to_mysql) \
+    .start()
 
-    trans_detail_write_stream.awaitTermination()
+trans_detail_write_stream.awaitTermination()
 
-    print("PySpark Structured Streaming with Kafka Demo Application Completed.")
+print("PySpark Structured Streaming with Kafka Demo Application Completed.")
